2643f8abcddc4bf8a1bd47812d32e72e9d0c357c
nobelrakib03 <nobelrakib>
2023-04-23 19:27:54 +0800

    product microservices grpc done

---
+++ GrpcMicroservices/ProductGrpc/Data/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductGrpc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductGrpc.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+
+    }
+}
